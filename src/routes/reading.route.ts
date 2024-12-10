3b26363a02cd69023ec33390026e2a0aafb744e9
stevenchan7 <0stevenchan0@gmail.com>
2024-12-10 21:02:27 +0800

    Update: remove auth middleware from get readings endpoint.

--- src/routes/reading.route.ts
+++ src/routes/reading.route.ts
@@ -7,7 +7,7 @@ import { validate } from '../helpers/validation.helper';
 
 const router = Router();
 
-router.get('/', authenticateAdmin, getReadings);
+router.get('/', getReadings);
 router.get('/:id', getReadingById);
 router.post('/', authenticateAdmin, upload.single('thumbnail'), createReadingValidationRules, validate, createReading);
 router.put('/:id', authenticateAdmin, upload.single('thumbnail'), editReadingValidationRules, validate, editReading);
