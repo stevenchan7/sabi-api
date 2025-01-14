ce76e5fe87784b8635547cf5fa4cd47112ac914c
stevenchan7 <0stevenchan0@gmail.com>
2025-01-14 19:00:55 +0800

    Update: model's associations.

--- src/helpers/sequelize.helper.ts
+++ src/helpers/sequelize.helper.ts
@@ -8,22 +8,22 @@ import UserAnswer from '../models/userAnswer.model';
 import UserAnswerGroup from '../models/userAnswerGroup.model';
 
 const associate = () => {
-  Group.hasMany(Question, { foreignKey: 'group_id' });
+  Group.hasMany(Question, { foreignKey: 'group_id', onDelete: 'CASCADE' });
   Question.belongsTo(Group, { foreignKey: 'group_id', as: 'group' });
 
   Question.hasMany(Option, { foreignKey: 'question_id', onDelete: 'CASCADE', as: 'options' });
-  Option.belongsTo(Question, { foreignKey: 'question_id', onDelete: 'CASCADE', as: 'question' });
+  Option.belongsTo(Question, { foreignKey: 'question_id', as: 'question' });
 
-  User.hasMany(UserAnswerGroup, { foreignKey: 'user_id' });
+  User.hasMany(UserAnswerGroup, { foreignKey: 'user_id', onDelete: 'CASCADE' });
   UserAnswerGroup.belongsTo(User, { foreignKey: 'user_id' });
 
-  UserAnswerGroup.hasMany(UserAnswer, { foreignKey: 'user_answer_group_id', as: 'userAnswers' });
+  UserAnswerGroup.hasMany(UserAnswer, { foreignKey: 'user_answer_group_id', as: 'userAnswers', onDelete: 'CASCADE' });
   UserAnswer.belongsTo(UserAnswerGroup, { foreignKey: 'user_answer_group_id', as: 'userAnswerGroup' });
 
-  Question.hasMany(UserAnswer, { foreignKey: 'question_id', as: 'userAnswers' });
+  Question.hasMany(UserAnswer, { foreignKey: 'question_id', as: 'userAnswers', onDelete: 'CASCADE' });
   UserAnswer.belongsTo(Question, { foreignKey: 'question_id', as: 'question' });
 
-  Option.hasMany(UserAnswer, { foreignKey: 'option_id' });
+  Option.hasMany(UserAnswer, { foreignKey: 'option_id', onDelete: 'CASCADE' });
   UserAnswer.belongsTo(UserAnswer, { foreignKey: 'option_id' });
 };
 
